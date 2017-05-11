a51966787e6b12204d1970b8bf01ae42eb2ca15c
AlexanderKuptsov <shon55398@gmail.com>
2017-05-12 01:55:28 +0300

    +menu, editor

---
+++ src/Data/Main/Editor.java
@@ -0,0 +1,66 @@
+package Data.Main;
+
+import Graphics.TileGrid;
+import Graphics.TileType;
+
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
+
+import static Helpers.Artist.HEIGHT;
+import static Helpers.Artist.TILE_SIZE;
+import static Helpers.LevelManager.saveMap;
+
+/**
+ * Created by shurik on 06.05.2017.
+ */
+public class Editor {
+
+    private TileGrid grid;
+    private int index;
+    private TileType[] types;
+
+    public Editor() {
+        this.grid = new TileGrid();
+        this.index = 0;
+
+        this.types = new TileType[6];
+        this.types[0] = TileType.Grass;
+        this.types[1] = TileType.Dirt;
+        this.types[2] = TileType.Water;
+        this.types[3] = TileType.Sand;
+        this.types[4] = TileType.Bush;
+        this.types[5] = TileType.Stones;
+    }
+
+    public void update() {
+        grid.draw();
+
+        // Mouse Input
+        if (Mouse.isButtonDown(0)) {
+            setTile();
+        }
+
+        // Keyboard Input
+        while (Keyboard.next()) {
+            if (Keyboard.getEventKey() == Keyboard.KEY_RIGHT && Keyboard.getEventKeyState()) {
+                moveIndex();
+            }
+            if (Keyboard.getEventKey() == Keyboard.KEY_S && Keyboard.getEventKeyState()) {
+                saveMap("newMarvelousMap1", grid);
+            }
+        }
+    }
+
+    private void setTile() {
+        grid.setTile((int) Math.floor(Mouse.getX() / TILE_SIZE),
+                (int) Math.floor((HEIGHT - Mouse.getY() - 1) / TILE_SIZE), types[index]);
+    }
+
+
+    private void moveIndex() {
+        index++;
+        if (index > types.length - 1) {
+            index = 0;
+        }
+    }
+}
---
+++ src/Data/Main/MainMenu.java
@@ -0,0 +1,39 @@
+package Data.Main;
+
+import Helpers.StateManager;
+import UI.UI;
+
+import org.newdawn.slick.opengl.Texture;
+
+import static Helpers.Artist.*;
+
+/**
+ * Created by shurik on 06.05.2017.
+ */
+public class MainMenu {
+    private Texture background;
+    private UI menuUI;
+
+    public MainMenu() {
+        background = quickLoad("mainmenu");
+        menuUI = new UI();
+        menuUI.addButton("Play", "play", WIDTH / 2 - 128, (int) (HEIGHT * 0.05f), 256, 256);
+        menuUI.addButton("Editor", "editor", WIDTH / 2 - 128, (int) (HEIGHT * 0.35f), 256, 256);
+        menuUI.addButton("Quit", "quit", WIDTH / 2 - 128, (int) (HEIGHT * 0.65f), 256, 256);
+    }
+
+    private void updateButtons() {
+        if (menuUI.isButtonClicked("Play"))
+            StateManager.setState(StateManager.GameState.GAME);
+        if (menuUI.isButtonClicked("Editor"))
+            StateManager.setState(StateManager.GameState.EDITOR);
+        if (menuUI.isButtonClicked("Quit"))
+            System.exit(0);
+    }
+
+    public void update() {
+        drawQuadTexture(background, 0, 0, 2048, 1024);
+        menuUI.draw();
+        updateButtons();
+    }
+}
